1df31e6c820479eeb5e8d7d92ea64e8c1a65403c
RDJulian <jruizd@fi.uba.ar>
2023-03-01 19:00:13 -0300

    feature: suma

---
+++ src/suma.py
@@ -0,0 +1,12 @@
+def suma(unNumero: float, otroNumero: float) -> float:
+    """
+    POST: Devuelve la suma de los numeros ingresados.
+    TESTS:
+    >>> suma(2, 5)
+    7
+    >>> suma(-3, 5)
+    2
+    >>> suma(2.4, 0)
+    2.4
+    """
+    return unNumero + otroNumero
