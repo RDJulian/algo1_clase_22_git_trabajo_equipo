e2e18a872f8bf7469ac496a2627fc0291ffea0f9
RDJulian <jruizd@fi.uba.ar>
2023-03-01 19:17:36 -0300

    feature: division entera

---
+++ src/divisionEntera.py
@@ -0,0 +1,10 @@
+def divisionEntera(dividendo: int, divisor: int) -> int:
+    """
+    POST: Devuelve el resultado de la division entera entre el dividendo y el divisor ingresados.
+    TESTS:
+    >>> divisionEntera(10, 3)
+    3
+    >>> divisionEntera(15, 1)
+    15
+    """
+    return dividendo // divisor
